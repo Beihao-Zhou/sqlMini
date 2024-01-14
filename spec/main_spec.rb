d1f67423a7fefb9fe88079be19b9f6d26bcd524e
Beihao Zhou <beihaozhou@gmail.com>
2024-01-14 09:38:22 -0500

    Support single-node btree

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,7 +1,10 @@
 describe 'database' do
+    before do
+      `rm -rf test.db`
+    end
     def run_script(commands)
       raw_output = nil
-      IO.popen("./db", "r+") do |pipe|
+      IO.popen("./db test.db", "r+") do |pipe|
         commands.each do |command|
           pipe.puts command
         end
@@ -84,4 +87,63 @@ describe 'database' do
         ])
     end
 
+    it 'keeps data after closing connection' do
+        result1 = run_script([
+          "insert 1 user1 person1@example.com",
+          ".exit",
+        ])
+        expect(result1).to match_array([
+          "db > Executed.",
+          "db > ",
+        ])
+        result2 = run_script([
+          "select",
+          ".exit",
+        ])
+        expect(result2).to match_array([
+          "db > (1, user1, person1@example.com)",
+          "Executed.",
+          "db > ",
+        ])
+    end
+
+    it 'prints constants' do
+        script = [
+            ".constants",
+            ".exit",
+        ]
+    result = run_script(script)
+
+    expect(result).to match_array([
+        "db > Constants:",
+        "ROW_SIZE: 293",
+        "COMMON_NODE_HEADER_SIZE: 6",
+        "LEAF_NODE_HEADER_SIZE: 10",
+        "LEAF_NODE_CELL_SIZE: 297",
+        "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+        "LEAF_NODE_MAX_CELLS: 13",
+        "db > ",
+        ])
+    end
+
+    it 'allows printing out the structure of a one-node btree' do
+      script = [3, 1, 2].map do |i|
+        "insert #{i} user#{i} person#{i}@example.com"
+      end
+      script << ".btree"
+      script << ".exit"
+      result = run_script(script)
+
+      expect(result).to match_array([
+        "db > Executed.",
+        "db > Executed.",
+        "db > Executed.",
+        "db > Tree:",
+        "leaf (size 3)",
+        "  - 0 : 3",
+        "  - 1 : 1",
+        "  - 2 : 2",
+        "db > "
+      ])
+    end
 end
